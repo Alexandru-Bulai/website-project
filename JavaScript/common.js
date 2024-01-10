0cfc5bec4d8825e556f57501927a292120d414fc
Alexandru Bulai <alexandru.iosif.bulai@gmail.com>
2024-01-10 14:10:33 +0000

    prb 3

--- JavaScript/common.js
+++ JavaScript/common.js
@@ -1,5 +1,5 @@
-const nav_button = document.getElementById("nav_button");
-const nav_bar = document.getElementById("nav_bar");
+const nav_button = document.getElementById("nav-button");
+const nav_bar = document.getElementById("nav-bar");
 
 nav_button.addEventListener('click', () => {
     nav_bar.classList.toggle('hidden');
