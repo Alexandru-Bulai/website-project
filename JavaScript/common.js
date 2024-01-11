ff8bdd7b417d2d59bc28d238c9b4aa9bfd91254e
Alexandru Bulai <alexandru.iosif.bulai@gmail.com>
2024-01-11 19:51:04 +0000

    geolocation

--- JavaScript/common.js
+++ JavaScript/common.js
@@ -1,6 +1,34 @@
+// Drop down menu functionality
 const nav_button = document.getElementById("nav_button");
 const nav_bar = document.getElementById("nav_bar");
 
 nav_button.addEventListener('click', () => {
-    nav_bar.classList.toggle('hidden');
+  nav_bar.classList.toggle('hidden');
 });
+
+// Geolocation Api
+const findState = () => {
+
+	const location = document.getElementById("location");
+
+	const allow = (position) => {
+		const latitude = position.coords.latitude;
+		const longitude = position.coords.longitude;
+
+		const locationApi = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
+
+		fetch(locationApi)
+		.then(response => response.json())
+		.then(data => {
+			location.textContent = data.principalSubdivision;
+		})
+	}
+
+	const deny = () => {
+		location.textContent = "Location?"
+	}
+
+		navigator.geolocation.getCurrentPosition(allow, deny);
+}
+
+window.addEventListener('load', findState);
