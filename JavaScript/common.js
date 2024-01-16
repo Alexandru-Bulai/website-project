81fb49a4a83c66aaad32d2a0b489656efc2ad21e
Alexandru Bulai <alexandru.iosif.bulai@gmail.com>
2024-01-16 19:34:35 +0000

    lintern js fix

--- JavaScript/common.js
+++ JavaScript/common.js
@@ -1,51 +1,51 @@
 // Drop down menu functionality
-const nav_button = document.getElementById("nav_button");
-const nav_bar = document.getElementById("nav_bar");
+const navButton = document.getElementById('navButton')
+const navBar = document.getElementById('navBar')
 
-nav_button.addEventListener('click', () => {
-  nav_bar.classList.toggle('hidden');
-});
+navButton.addEventListener('click', () => {
+  navBar.classList.toggle('hidden')
+})
 
 // Geolocation Api
 const findMyLocation = () => {
-	const location = document.getElementById("location");
-	if (!location) {
-		console.error("location could not be found on the page");
-		return;
-	}
+  const location = document.getElementById('location')
+  if (!location) {
+    console.error('location could not be found on the page')
+    return
+  }
 
-	const allow = (position) => {
-		const latitude = position.coords.latitude;
-		const longitude = position.coords.longitude;
+  const allow = (position) => {
+    const latitude = position.coords.latitude
+    const longitude = position.coords.longitude
 
-		const locationApi = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
+    const locationApi = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
 
-		fetch(locationApi)
-			.then(response => {
-				if (!response.ok) {
-					throw new Error('Network response was rejected');
-				}
-				return response.json();
-			})
-			.then(data => {
-				location.textContent = data.principalSubdivision;
-			})
-			.catch(error => {
-				console.error('Error fetching the location:', error);
-				location.textContent = "Not found";
-			});
-	}
+    fetch(locationApi)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was rejected')
+        }
+        return response.json()
+      })
+      .then(data => {
+        location.textContent = data.principalSubdivision
+      })
+      .catch(error => {
+        console.error('Error fetching the location:', error)
+        location.textContent = 'Not found'
+      })
+  }
 
-	const deny = () => {
-		location.textContent = "Permission denied.";
-	}
+  const deny = () => {
+    location.textContent = 'Permission denied.'
+  }
 
-	navigator.geolocation.getCurrentPosition(allow, deny);
+  navigator.geolocation.getCurrentPosition(allow, deny)
 }
 
-const locationButton = document.getElementById("location-button");
+const locationButton = document.getElementById('location-button')
 if (locationButton) {
-	locationButton.addEventListener('click', findMyLocation);
+  locationButton.addEventListener('click', findMyLocation)
 } else {
-	console.error("The location button could not be found on the page.");
+  console.error('The location button could not be found on the page.')
 }
