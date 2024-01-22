0e2eff3a498b0f633929bdd9402b21d336818b85
Alexandru Bulai <alexandru.iosif.bulai@gmail.com>
2024-01-22 12:33:15 +0000

    lintern fix

--- public/JavaScript/common.js
+++ public/JavaScript/common.js
@@ -1,23 +1,23 @@
 // Drop down menu functionality
-function toggleElements(buttonSelector, elementSelectors) {
-	const button = document.querySelector(buttonSelector);
+function toggleElements (buttonSelector, elementSelectors) {
+  const button = document.querySelector(buttonSelector)
 
-	if (button && Array.isArray(elementSelectors)) {
-		button.addEventListener('click', () => {
-			elementSelectors.forEach(selector => {
-				const element = document.querySelector(selector);
-				if (element) {
-					element.classList.toggle('hidden');
-				} else {
-					element.classList.toggle('flex');
-				}
-			});
-		});
-	} else {
-		console.warn('Button not found or invalid element selectors');
-	}
+  if (button && Array.isArray(elementSelectors)) {
+    button.addEventListener('click', () => {
+      elementSelectors.forEach(selector => {
+        const element = document.querySelector(selector)
+        if (element) {
+          element.classList.toggle('hidden')
+        } else {
+          element.classList.toggle('flex')
+        }
+      })
+    })
+  } else {
+    console.warn('Button not found or invalid element selectors')
+  }
 }
-toggleElements('#navButton', ['#navBar']);
-toggleElements('#filtersButton', ['#filtersButton', '#filtersMenu', '#closeFilters']);
-toggleElements('#closeFilters', ['#filtersButton', '#filtersMenu', '#closeFilters']);
-toggleElements('#animalsFilterButton', ['#singleAnimalFilter']);
+toggleElements('#navButton', ['#navBar'])
+toggleElements('#filtersButton', ['#filtersButton', '#filtersMenu', '#closeFilters'])
+toggleElements('#closeFilters', ['#filtersButton', '#filtersMenu', '#closeFilters'])
+toggleElements('#animalsFilterButton', ['#singleAnimalFilter'])
--- public/JavaScript/home.js
+++ public/JavaScript/home.js
@@ -1,42 +1,42 @@
 const findMyLocation = () => {
-    const location = document.getElementById('location');
-    if (!location) {
-        console.error('location could not be found on the page');
-        return;
-    }
+  const location = document.getElementById('location')
+  if (!location) {
+    console.error('location could not be found on the page')
+    return
+  }
 
-    const allow = (position) => {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
+  const allow = (position) => {
+    const latitude = position.coords.latitude
+    const longitude = position.coords.longitude
 
-        const locationApi = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
+    const locationApi = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
 
-        fetch(locationApi)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                location.textContent = data.principalSubdivision;
-            })
-            .catch(error => {
-                console.error('Error fetching the location:', error);
-                location.textContent = 'Location not found';
-            });
-    };
+    fetch(locationApi)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+        return response.json()
+      })
+      .then(data => {
+        location.textContent = data.principalSubdivision
+      })
+      .catch(error => {
+        console.error('Error fetching the location:', error)
+        location.textContent = 'Location not found'
+      })
+  }
 
-    const deny = () => {
-        location.textContent = 'Permission denied';
-    };
+  const deny = () => {
+    location.textContent = 'Permission denied'
+  }
 
-    navigator.geolocation.getCurrentPosition(allow, deny);
-};
+  navigator.geolocation.getCurrentPosition(allow, deny)
+}
 
-const locationButton = document.getElementById('location-button');
+const locationButton = document.getElementById('location-button')
 if (locationButton) {
-    locationButton.addEventListener('click', findMyLocation);
+  locationButton.addEventListener('click', findMyLocation)
 } else {
-    console.error('The location button could not be found on the page.');
+  console.error('The location button could not be found on the page.')
 }
