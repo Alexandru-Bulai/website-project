b35ad54fd71d4db9f8d1c06bb237258e23a02eca
Alexandru Bulai <alexandru.iosif.bulai@gmail.com>
2024-01-28 14:41:48 +0000

    lintern fix

--- public/JavaScript/gallery.js
+++ public/JavaScript/gallery.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-	let selectedAnimalType = null;
-	let galleryItems= document.querySelectorAll('.gallery-items');
+  let selectedAnimalType = null
+  const galleryItems = document.querySelectorAll('.gallery-items')
 
   // Function to hide all gallery items
   const hideAllAnimals = (elements) => {
@@ -9,24 +9,24 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
   // Function to display selected animal
-	const showSelectedAnimals = () => {
-		hideAllAnimals(document.querySelectorAll('.gallery-items'));
-		if (selectedAnimalType) {
-			galleryItems.forEach(item => {
-				if (item.dataset.animalType === selectedAnimalType) {
-					item.style.display = 'block';
-				}
-			});
-		}
-	};
+  const showSelectedAnimals = () => {
+    hideAllAnimals(document.querySelectorAll('.gallery-items'))
+    if (selectedAnimalType) {
+      galleryItems.forEach(item => {
+        if (item.dataset.animalType === selectedAnimalType) {
+          item.style.display = 'block'
+        }
+      })
+    }
+  }
   // Target animal type
-	function setSelectedAnimalType(type) {
-		selectedAnimalType = type;
-	}
+  function setSelectedAnimalType (type) {
+    selectedAnimalType = type
+  }
 
   document.querySelectorAll('[data-animal-type]').forEach(filterOption => {
     filterOption.addEventListener('click', () => {
-			setSelectedAnimalType(filterOption.dataset.animalType);
+      setSelectedAnimalType(filterOption.dataset.animalType)
       console.log(`Selected animal type: ${selectedAnimalType}`)
     })
   })
