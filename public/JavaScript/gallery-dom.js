b43e3bdfb72d1a4c601ed692c03a92a7fc68242c
Alex <alexandru.bulai1@proton.me>
2024-02-06 17:39:08 +0200

    add gallery-dom.js for dom content

---
+++ public/JavaScript/gallery-dom.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Event listeners for animal filter buttons
+  document.querySelectorAll('#singleAnimalFilter div[data-type]').forEach(activeFilterElements => {
+    activeFilterElements.addEventListener('click', function () {
+      // Toggle 'active' class on click
+      this.classList.toggle('active')
+    })
+  })
+  const applyFiltersButton = document.querySelector('#apply-filters')
+
+  applyFiltersButton.addEventListener('click', function (event) {
+    event.preventDefault()
+
+    // Retrieve selected filter values
+    const genderFilter = document.querySelector('input[name="gender"]:checked')?.value
+    const typeFilters = document.querySelectorAll('#singleAnimalFilter div[data-type].active')
+    const ratingFilter = document.querySelector('.star-rating input[type="radio"]:checked')?.getAttribute('data-rating')
+
+    // Prepare the selected filters object
+    const userSelectedFilters = {
+      gender: genderFilter ? [genderFilter] : [],
+      type: Array.from(typeFilters, filter => filter.getAttribute('data-type')),
+      rating: ratingFilter ? [parseInt(ratingFilter)] : []
+    }
+
+    const activeFilters = generateFilterOptions(userSelectedFilters)
+
+    console.log('Applying filters:', activeFilters) // Debugging line
+    applyGalleryFilter(activeFilters)
+  })
+})
--- public/JavaScript/gallery.js
+++ public/JavaScript/gallery.js
@@ -1,35 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Event listeners for animal filter buttons
-  document.querySelectorAll('#singleAnimalFilter div[data-type]').forEach(filterElementsActive => {
-    filterElementsActive.addEventListener('click', function () {
-      // Toggle 'active' class on click
-      this.classList.toggle('active')
-    })
-  })
-  const applyFiltersButton = document.querySelector('#apply-filters')
-
-  applyFiltersButton.addEventListener('click', function (event) {
-    event.preventDefault()
-
-    // Retrieve selected filter values
-    const genderFilter = document.querySelector('input[name="gender"]:checked')?.value
-    const typeFilters = document.querySelectorAll('#singleAnimalFilter div[data-type].active')
-    const ratingFilter = document.querySelector('.star-rating input[type="radio"]:checked')?.getAttribute('data-rating')
-
-    // Prepare the selected filters object
-    const userSelectedFilters = {
-      gender: genderFilter ? [genderFilter] : [],
-      type: Array.from(typeFilters, filter => filter.getAttribute('data-type')),
-      rating: ratingFilter ? [parseInt(ratingFilter)] : []
-    }
-
-    const activeFilters = generateFilterOptions(userSelectedFilters)
-
-    console.log('Applying filters:', activeFilters) // Debugging line
-    applyGalleryFilter(activeFilters)
-  })
-})
-
 /**
  *
  * @param {Number} imagesInDom
@@ -106,4 +74,4 @@ function applyGalleryFilter (activeFilters) {
   })
 }
 
-module.exports = { reportGalleryStats, generateFilterOptions }
+module.exports = { reportGalleryStats, generateFilterOptions, applyGalleryFilter }
