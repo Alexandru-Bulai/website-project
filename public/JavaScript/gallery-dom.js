12dedc14aaa94fcdce740ff821ca594d6f9540f0
Alex <alexandru.bulai1@proton.me>
2024-02-07 16:32:34 +0200

    adjust spacing for lintern

--- public/JavaScript/gallery-dom.js
+++ public/JavaScript/gallery-dom.js
@@ -7,13 +7,13 @@ document.addEventListener('DOMContentLoaded', function () {
       this.classList.toggle('selected')
     })
   })
-  const applyFiltersButton = document.querySelector('#apply-filters');
+  const applyFiltersButton = document.querySelector('#apply-filters')
 
   applyFiltersButton.addEventListener('click', function (event) {
-    event.preventDefault();
+    event.preventDefault()
 
     // Retrieve selected filter values
-    const genderFilter = document.querySelector('input[name="gender"]:checked')?.value;
+    const genderFilter = document.querySelector('input[name="gender"]:checked')?.value
     const typeFilters = document.querySelectorAll('#singleAnimalFilter div[data-type].selected')
     const ratingFilter = document.querySelector('.star-rating input[type="radio"]:checked')?.getAttribute('data-rating')
 
@@ -24,32 +24,32 @@ document.addEventListener('DOMContentLoaded', function () {
       rating: ratingFilter ? [parseInt(ratingFilter)] : []
     }
 
-    const activeFilters = generateFilterOptions(userSelectedFilters);
+    const activeFilters = generateFilterOptions(userSelectedFilters)
 
     console.log('Applying filters:', activeFilters) // Debugging line
     applyGalleryFilter(activeFilters)
 
-		// Reset Filters after applying
-		document.querySelectorAll('#singleAnimalFilter div[data-type].selected').forEach(element => {
-			element.classList.remove('selected');
-		});
+    // Reset Filters after applying
+    document.querySelectorAll('#singleAnimalFilter div[data-type].selected').forEach(element => {
+      element.classList.remove('selected')
+    })
 
-		resetFilters();
+    resetFilters()
   })
-	function resetFilters() {
+  function resetFilters () {
     // Reset gender filter
     document.querySelectorAll('input[name="gender"]').forEach(radio => {
-      radio.checked = false;
-    });
+      radio.checked = false
+    })
 
     // Reset type filters visual cue
     document.querySelectorAll('#singleAnimalFilter div[data-type].selected').forEach(typeFilter => {
-      typeFilter.classList.remove('selected');
-    });
+      typeFilter.classList.remove('selected')
+    })
 
     // Reset rating filter
-		document.querySelectorAll('.star-rating input[type="radio"]').forEach(radio => {
-			radio.checked = false;
-		});
+    document.querySelectorAll('.star-rating input[type="radio"]').forEach(radio => {
+      radio.checked = false
+    })
   }
-});
+})
