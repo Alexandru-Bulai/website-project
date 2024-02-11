e77e73ff80a7fe2a824f3ae37d41931e1852f033
Vladi Iancu <vladi.iancu@gmail.com>
2024-02-11 19:50:09 +0200

    lint fix

--- test/galleryDom.test.js
+++ test/galleryDom.test.js
@@ -3,7 +3,7 @@ const jsdom = require('jsdom')
 const { JSDOM } = jsdom
 const { displayGalleryItemsNum } = require('../public/JavaScript/gallery-dom.js')
 
-describe("Gallery Counter", () => {
+describe('Gallery Counter', () => {
   it('sets the count to 1 when having 1 item gallery hidden and one shown', () => {
     const dom = new JSDOM(`
       <div class="gallery-items" style="display: block;"></div>
@@ -11,16 +11,16 @@ describe("Gallery Counter", () => {
       <div id="number-items"></div>
     `)
     global.document = dom.window.document
-  
+
     // Mock the reportGalleryStats function globally
     global.reportGalleryStats = (count) => `Count: ${count}`
-  
+
     // Call the function with the mock DOM
     const message = displayGalleryItemsNum('.gallery-items')
-  
+
     expect(message).toBe('Count: 1')
   })
-  
+
   it('sets the count to 3 when having 3 (all) items set to shown', () => {
     const dom = new JSDOM(`
       <div class="gallery-items" style="display: block;"></div>
@@ -31,7 +31,7 @@ describe("Gallery Counter", () => {
     global.document = dom.window.document
     global.reportGalleryStats = (count) => `Count: ${count}`
     const message = displayGalleryItemsNum('.gallery-items')
-  
+
     expect(message).toBe('Count: 3')
   })
 
@@ -44,8 +44,7 @@ describe("Gallery Counter", () => {
     global.document = dom.window.document
     global.reportGalleryStats = (count) => `Count: ${count}`
     const message = displayGalleryItemsNum('.gallery-items')
-  
+
     expect(message).toBe('Count: 0')
   })
 })
-
